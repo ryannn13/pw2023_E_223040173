8753b09834b9f015dc8a1c6fbcf9831e1a9afb2c
ryannn13 <ryan.hidayat0604@gmail.com>
2023-03-11 09:51:40 +0700

    Tugas Kuliah

---
+++ tugas/tugas4/4b.php
@@ -0,0 +1,31 @@
+<?php 
+$perangkat = ["Motherboard","Processor","Hard Disk","PC Coller","VGA Card","SSD"];
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 4b</title>
+</head>
+<body>
+    <h2>Macam-macam perangkat keras komputer</h2>
+    <ol>
+        <?php foreach ($perangkat as $p) {?>
+        <li><?= $p; ?></li>  
+        <?php } ?>
+    </ol>
+    <?php 
+    $perangkat[] = "Card Reader";
+    $perangkat[] = "Modem";
+    sort($perangkat);
+    ?>
+    <h2>Macam-macam perangkat keras komputer baru</h2>
+    <ol>
+        <?php foreach ($perangkat as $p) {?>
+        <li><?= $p; ?></li>  
+        <?php } ?>
+    </ol>
+</body>
+</html>
